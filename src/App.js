0d282886e73d9d7a019afe5c74e62fefeb913010
raajv <raaj.vardhan@gmail.com>
2021-11-02 13:14:18 +0530

    added lat long func

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+export default class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state={
+      cityValue : '',
+      
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleClick =async() =>{
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityValue}&format=json`;
+    let response = await axios.get(url)
+    console.log (response.data)
+    this.setState({location:response.data[0]})
+
+  }
+
+handleChange =(e)=>{
+ this.setState({cityValue:e.target.value})
+}
+  render() {
+    return (
+      <>
+      <InputGroup className="mb-3">
+    <FormControl
+      onChange ={this.handleChange}
+      placeholder="Enter City Name"
+      aria-label="Enter City Name"
+      aria-describedby="basic-addon2"
+      value={this.state.cityValue}
+    />
+    <Button onClick ={this.handleClick}variant="outline-secondary" id="button-addon2">
+      Explore
+    </Button>
+  </InputGroup>
+  {this.state.location && 
+    <Card style={{ width: '18rem' }}>
+    <Card.Body>
+    <Card.Title>{this.state.location.display_name}</Card.Title>
+    <Card.Text>
+    The latitude and longitude is {this.state.location.lat} , {this.state.location.lon}
+    </Card.Text>
+    
+    </Card.Body>
+    </Card>}
+      </>
+    
+    
+)
+}
 }
 
-export default App;
