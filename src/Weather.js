d4aac9a8ce0f7e20349c9e4afdd3f1ed9bd9d2ff
raajv <raaj.vardhan@gmail.com>
2021-11-03 23:44:45 +0530

    added functionality of getting weather from dummy

--- src/Weather.js
+++ src/Weather.js
@@ -1,34 +1,39 @@
 import React,{ Component } from 'react';
 import axios from 'axios';
-
+import Alert from 'react-bootstrap/Alert'
 export default class Weather extends Component{
   
   constructor(props){
     super(props);
     this.state={
-      weather:{}
-      
+      error:false,
+      weather:[]
     }
   }
   getWeather =async()=>{
-      const url = `${process.env.REACT_APP_SERVER_URL}/weather`;
+      const url = `${process.env.REACT_APP_SERVER_URL}/weather?city=${this.props.city}`;
+      try{
       let response = await axios.get(url);
-      this.setState({weather:response.data})
-      console.log(response.data);
-      this.findCity(response.data)
+      this.setState({weather:response.data[0]})
+      console.log(response.data);}
+      catch(err){
+      this.setState({error:true})
+      }
   }
 
-  findCity =(res)=>{
-    console.log(this.props.city)
-   let newCity= res.find(({ city_name }) => city_name === this.props.city)
-    this.setState({cityWeather:newCity})
-    console.log(newCity)
-  }
+
   render(){
     return(
       <>
-      <button onClick={this.getWeather}>click me</button>
-      <h1>{this.state.city}</h1>
+      {this.state.error && <Alert variant="danger" onClose={() => this.setState({error:false})} dismissible>
+     <Alert.Heading>Oh! No data available for this city</Alert.Heading>
+     <p>
+       Please enter Seattle , Paris or Amman!!
+     </p>
+   </Alert>}
+      <button onClick={this.getWeather}>click to see the weather for {this.props.city}!</button>
+      {this.state.weather &&  <p> the weather is : min temp :{this.state.weather.min_temp} max temp: {this.state.weather.max_temp} clouds: {this.state.weather.description} </p>}
+      
       </>
     )
   }
